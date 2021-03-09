5e2e148cbc403d76966147b9c64637cd85611377
Mike Schulze <mschulze@datameer.com>
2021-03-09 20:22:47 +0100

    append

--- addons/gdUnit3/src/report/GdUnitReportSummary.gd
+++ addons/gdUnit3/src/report/GdUnitReportSummary.gd
@@ -1,167 +1,69 @@
 class_name GdUnitReportSummary
 extends Reference
 
-const FOLDER_REPORT_TEMPLATE = """
-								<tr>
-									<td><a class="${report_state}" href="folders/${path}.html">${path}</a></td>
-									<td>${test_count}</td>
-									<td>${failure_count}</td>
-									<td>${orphan_count}</td>
-									<td>${duration}</td>
-									<td class="${report_state}">${success_percent}</td>
-								</tr>
-"""
-const REPORT_BY_FOLDER = "${testsuite_report_by_folders}"
-
-
-const REPORT_BY_SUITES = "${testsuite_report_by_suites}"
-
-# the report state success, error, warning
-const PATTERN_REPORT_STATE = "${report_state}"
-const PATTERN_PATH = "${path}"
-const PATTERN_TESTSUITE_COUNT = "${suite_count}"
-const PATTERN_TEST_COUNT = "${test_count}"
-const PATTERN_FAILURE_COUNT = "${failure_count}"
-const PATTERN_ORPHAN_COUNT = "${orphan_count}"
-const PATTERN_DURATION = "${duration}"
-const PATTERN_SUCCESS_PERCENT = "${success_percent}"
-
-
+var _resource_path :String
+var _name :String
 var _suite_count := 0
 var _test_count := 0
 var _failure_count := 0
 var _error_count := 0
 var _orphan_count := 0
 var _duration := 0
-var _reports:Dictionary = Dictionary()
+var _reports:Array = Array()
 
-func _init(testsuites :int, tests :int):
-	_suite_count = testsuites
-	_test_count = tests
-	_duration = OS.get_system_time_msecs()
+func name() -> String:
+	return _name
 
-func stop_timer() -> void:
-	_duration = OS.get_system_time_msecs() - _duration
+func path() -> String:
+	return _resource_path.get_base_dir().replace("res://", "")
 
 func suite_count() -> int:
-	return _suite_count
+	return _reports.size()
 
 func test_count() -> int:
-	return _test_count 
+	var count := _test_count
+	for report in _reports:
+		count += report.test_count()
+	return count
 
 func failure_count() -> int:
-	return _failure_count
+	var count := _failure_count
+	for report in _reports:
+		count += report.failure_count()
+	return count
 
 func orphan_count() -> int:
-	return _orphan_count
+	var count := _orphan_count
+	for report in _reports:
+		count += report.orphan_count()
+	return count
 
 func duration() -> int:
-	return _duration
+	var count := _duration
+	for report in _reports:
+		count += report.duration()
+	return count
 
-func calculate_state(failure_count :int, orphan_count :int) -> String:
+func add_report(report :GdUnitReportSummary) -> void:
+	_reports.append(report)
+
+func report_state() -> String:
+	return calculate_state(failure_count(), orphan_count())
+
+func succes_rate() -> String:
+	return calculate_succes_rate(test_count(), failure_count())
+
+static func calculate_state(failure_count :int, orphan_count :int) -> String:
 	if failure_count > 0:
 		return "failure"
 	if orphan_count > 0:
 		return "warning"
 	return "success"
 
-func report_state() -> String:
-	return calculate_state(_failure_count, _orphan_count)
-
-func succes_rate() -> String:
-	return calculate_succes_rate(_test_count, _failure_count)
-
-func calculate_succes_rate(test_count :int, failure_count: int) -> String:
+static func calculate_succes_rate(test_count :int, failure_count: int) -> String:
 	if failure_count == 0:
 		return "100%"
 	return "%d" % ((test_count-failure_count) * 100 / test_count) + "%"
 
-func add_report(report) -> void:
-	_reports[report.name()] = report
-
-func add_testsuite_summary(suite_name :String, failures :int, errors :int, orphans :int, duration :int):
-	var suite_report = _reports[suite_name]
-	suite_report.update(failures, errors, orphans, duration)
-
-func add_testcase_report(suite_name :String, test_name :String, is_failed :bool, orphans :int, reports :Array, duration :int):
-	var suite_report = _reports[suite_name]
-	suite_report.add_test_report(test_name, is_failed, orphans, reports, duration)
-
-func write_html_report(report_dir :String) -> String:
-	var template := load_template("res://addons/gdUnit3/src/report/template/index.html")
-	var to_write = fill_summary(template, self)
-	to_write = fill_folder_reports(to_write)
-	to_write = fill_suite_reports(to_write, report_dir)
-
-	
-	# write report
-	var file := File.new()
-	file.open("%s/index.html" % report_dir, File.WRITE)
-	file.store_string(to_write)
-	file.close()
-	
-	GdUnitTools.copy_directory("res://addons/gdUnit3/src/report/template/css/", report_dir)
-	return "%s/index.html" % report_dir
-
-func fill_summary(template :String, report :GdUnitReportSummary) -> String:
-	return template\
-		.replace(PATTERN_TESTSUITE_COUNT, str(report.suite_count()))\
-		.replace(PATTERN_TEST_COUNT, str(report.test_count()))\
-		.replace(PATTERN_FAILURE_COUNT, str(report.failure_count()))\
-		.replace(PATTERN_ORPHAN_COUNT, str(report.orphan_count()))\
-		.replace(PATTERN_DURATION, LocalTime.elapsed(report.duration()))\
-		.replace(PATTERN_SUCCESS_PERCENT, calculate_succes_rate(report.test_count(), report.failure_count()))\
-		.replace(PATTERN_REPORT_STATE, report_state())
-
-func fill_folder_reports(template :String) -> String:
-	var report_rows := PoolStringArray()
-	var by_paths := merged_report_by_path(_reports.values())
-	
-	for path in by_paths.keys():
-		var reports :Array = by_paths.get(path)
-		var test_count := 0
-		var failure_count := 0
-		var orphan_count := 0
-		var duration := 0
-		for report in reports:
-			test_count += report.test_count()
-			failure_count += report.failure_count()
-			orphan_count += report.orphan_count()
-			duration += report.duration()
-		
-		var folder_report := FOLDER_REPORT_TEMPLATE\
-			.replace(PATTERN_REPORT_STATE, calculate_state(failure_count, orphan_count))\
-			.replace(PATTERN_PATH, path)\
-			.replace(PATTERN_TEST_COUNT, str(test_count))\
-			.replace(PATTERN_FAILURE_COUNT, str(failure_count))\
-			.replace(PATTERN_ORPHAN_COUNT, str(orphan_count))\
-			.replace(PATTERN_DURATION, LocalTime.elapsed(duration))\
-			.replace(PATTERN_SUCCESS_PERCENT, calculate_succes_rate(test_count, failure_count))
-		
-		report_rows.append(folder_report)
-	by_paths.clear()
-	return template.replace(REPORT_BY_FOLDER, report_rows.join("\n"))
-
-func fill_suite_reports(template :String, report_dir :String) -> String:
-	var report_rows := PoolStringArray()
-	for report in _reports.values():
-		report_rows.append(report.create_summary(report_dir))
-		
-	return template.replace(REPORT_BY_SUITES, report_rows.join("\n"))
-
-static func merged_report_by_path(reports :Array) -> Dictionary:
-	
-	var by_path := Dictionary()
-	for report in reports:
-		var suite_path :String = report.path()
-		var suite_report :Array = by_path.get(suite_path, Array())
-		suite_report.append(report)
-		by_path[suite_path] = suite_report
-	return by_path
-
-func load_template(template_name :String) -> String:
-	var file := File.new()
-	file.open(template_name, File.READ)
-	var template := file.get_as_text()
-	file.close()
-	return template
+func create_summary(report_dir :String) -> String:
+	return ""
--- addons/gdUnit3/src/report/GdUnitTestCaseReport.gd
+++ addons/gdUnit3/src/report/GdUnitTestCaseReport.gd
@@ -1,38 +1,19 @@
 class_name GdUnitTestCaseReport
-extends Reference
+extends GdUnitReportSummary
 
+var _failure_reports :Array
 
-const SUITE_TESTS_REPORT_TEMPLATE = """
-								<tr>
-									<td class="${report_state}">${testcase_name}</td>
-									<td>${orphan_count}</td>
-									<td>${duration}</td>
-								</tr>
-"""
-
-var _name :String
-var _is_failed :bool
-var _orphans :int
-var _reports :Array
-var _duration :int
-
-func _init(test_name :String, is_failed :bool, orphans :int, reports :Array, duration :int):
+func _init(test_name :String, is_failed :bool, orphans :int, failure_reports :Array, duration :int):
 	_name = test_name
-	_is_failed = is_failed
-	_orphans = orphans
-	_reports = reports
+	_test_count = 1
+	_failure_count = is_failed
+	_orphan_count = orphans
+	_failure_reports = failure_reports
 	_duration = duration
 
-func create_summary() -> String:
-	return SUITE_TESTS_REPORT_TEMPLATE\
-		.replace("${report_state}", report_state())\
-		.replace("${testcase_name}", _name)\
-		.replace("${orphan_count}", str(_orphans))\
-		.replace("${duration}", LocalTime.elapsed(_duration))
-
-func report_state() -> String:
-	if _is_failed:
-		return "failure"
-	if _orphans > 0:
-		return "warning"
-	return "success"
+func create_record(report_dir :String) -> String:
+	return GdUnitHtmlPatterns.TABLE_RECORD_TESTCASE\
+		.replace(GdUnitHtmlPatterns.REPORT_STATE, report_state())\
+		.replace(GdUnitHtmlPatterns.TESTCASE_NAME, name())\
+		.replace(GdUnitHtmlPatterns.ORPHAN_COUNT, str(orphan_count()))\
+		.replace(GdUnitHtmlPatterns.DURATION, LocalTime.elapsed(_duration))
