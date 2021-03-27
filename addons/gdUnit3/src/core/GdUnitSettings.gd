806eea2a2622f96bac9d2870937591d296e006dd
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2021-03-27 17:11:48 +0100

    GD-24: Fix inital loading of GdUnit settings
    - related to https://github.com/godotengine/godot/pull/46386
    - needs to update Godot to version  3.2.4.

--- addons/gdUnit3/src/core/GdUnitSettings.gd
+++ addons/gdUnit3/src/core/GdUnitSettings.gd
@@ -1,30 +1,37 @@
+tool
 class_name GdUnitSettings
 extends Reference
 
-const CATEGORY = "gdunit"
+const CATEGORY = "gdunit3"
 const REPORT_ERROR_NOTIFICATIONS = CATEGORY + "/report/error_notification"
 const REPORT_ERROR_INFO = CATEGORY + "/report/info"
 
 static func load_settings():
-	ProjectSettings.clear(REPORT_ERROR_NOTIFICATIONS)
-
-	#if not ProjectSettings.has_setting(CATEGORY):
-		
-	var error_notification = {
-		"name": REPORT_ERROR_NOTIFICATIONS,
-		"type": TYPE_BOOL,
-		"hint": PROPERTY_HINT_NONE,
-		"hint_string": "Enable to report push error notifications"
-	}
-	ProjectSettings.set(REPORT_ERROR_NOTIFICATIONS, false)
-	ProjectSettings.add_property_info(error_notification)
-	ProjectSettings.set_initial_value(REPORT_ERROR_NOTIFICATIONS, false)
+	var is_settings_changed := false
 	
-
-	var ret = ProjectSettings.save()
-	if ret != OK:
-		prints(GdUnitTools.error_as_string(ret))
-		push_error(GdUnitTools.error_as_string(ret))
+	if not ProjectSettings.has_setting(REPORT_ERROR_NOTIFICATIONS):
+		prints("GdUnit3: Set inital settings", REPORT_ERROR_NOTIFICATIONS)
+		var error_notification = {
+			"name": REPORT_ERROR_NOTIFICATIONS,
+			"type": TYPE_BOOL,
+			"hint": PROPERTY_HINT_NONE,
+			"hint_string": "Enable to report push error notifications"
+		}
+		ProjectSettings.set(REPORT_ERROR_NOTIFICATIONS, false)
+		ProjectSettings.add_property_info(error_notification)
+		ProjectSettings.set_initial_value(REPORT_ERROR_NOTIFICATIONS, false)
+		ProjectSettings.set_setting(REPORT_ERROR_NOTIFICATIONS, false)
+		is_settings_changed = true
+	
+	if is_settings_changed: 
+		var ret = ProjectSettings.save()
+		if ret != OK:
+			push_error(GdUnitTools.error_as_string(ret))
+			return
+		else:
+			prints("GdUnit3: Settings succesfully saved.")
+	else:
+		prints("GdUnit3: Settings successfully loaded.")
 
 static func is_report_push_errors() -> bool:
 	if ProjectSettings.has_setting(REPORT_ERROR_NOTIFICATIONS):
