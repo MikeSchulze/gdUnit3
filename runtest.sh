2ccdc4dba0d564d272dd4394da1ef4ee69e357cf
Nullpointer <mikeschulze.lpz@kabelmail.de>
2021-05-31 19:28:46 +0200

    GD-74: Added github action to run selftest on push event

--- runtest.sh
+++ runtest.sh
@@ -7,6 +7,6 @@ if [ -z "$GODOT_BIN" ]; then
 fi
 
 $GODOT_BIN --no-window -s -d ./addons/gdUnit3/bin/GdUnitCmdTool.gd $*
-declare -i exit_code=$?
+exit_code=$?
 $GODOT_BIN --no-window --quiet -s -d ./addons/gdUnit3/bin/GdUnitCopyLog.gd $* > /dev/null
 exit $exit_code
