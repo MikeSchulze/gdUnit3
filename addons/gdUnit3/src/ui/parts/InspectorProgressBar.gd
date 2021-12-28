d32fdbba50ea282844cd06c1bdeb528f5cac4a27
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2021-12-28 17:35:06 +0100

    GD-199: Failing test suite on stage `after` now correct reports on inspector
    - update progress bar to status failed if stage after reports failures
    - include failing test suite to failure navigaton (next, prevoius)

--- addons/gdUnit3/src/ui/parts/InspectorProgressBar.gd
+++ addons/gdUnit3/src/ui/parts/InspectorProgressBar.gd
@@ -7,16 +7,16 @@ onready var style :StyleBoxFlat = bar.get("custom_styles/fg")
 
 onready var _signal_handler :SignalHandler = GdUnitSingleton.get_singleton(SignalHandler.SINGLETON_NAME)
 
-
 func _ready():
 	_signal_handler.register_on_gdunit_events(self, "_on_event")
 	style.bg_color = Color.darkgreen
 
+func progress_init(max_value :int) -> void:
+	bar.value = 0
+	bar.max_value = max_value
+	style.bg_color = Color.darkgreen
+
 func progress_update(value :int, failed :int, max_value :int = -1) -> void:
-	if max_value != -1:
-		bar.max_value = max_value
-	if value == 0:
-		bar.value = 0
 	bar.value += value
 	status.text = str(bar.value) + ":" + str(bar.max_value)
 	# if faild change color to red
@@ -27,8 +27,7 @@ func progress_update(value :int, failed :int, max_value :int = -1) -> void:
 func _on_event(event :GdUnitEvent) -> void:
 	match event.type():
 		GdUnitEvent.INIT:
-			style.bg_color = Color.darkgreen
-			progress_update(0, 0, event.total_count())
+			progress_init(event.total_count())
 		GdUnitEvent.TESTCASE_BEFORE:
 			pass
 		GdUnitEvent.TESTCASE_AFTER:
@@ -36,4 +35,5 @@ func _on_event(event :GdUnitEvent) -> void:
 		GdUnitEvent.TESTSUITE_BEFORE:
 			pass
 		GdUnitEvent.TESTSUITE_AFTER:
+			progress_update(0, event.is_failed())
 			pass
