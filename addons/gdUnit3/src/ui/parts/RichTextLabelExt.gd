71db0ac6f4bc6c1222906662556ce0e0ad4c3046
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2022-01-23 13:05:03 +0100

    GD-211: Fix failure highlighting on GdUnit Console
    - The failure highlighting was placed incorrectly when the text is warped.
      Now the minimum text field length is set to the maximum text length to avoid line breaks.

--- addons/gdUnit3/src/ui/parts/RichTextLabelExt.gd
+++ addons/gdUnit3/src/ui/parts/RichTextLabelExt.gd
@@ -10,8 +10,9 @@ class_name RichTextLabelExt
 var _effect :RichTextEffectBackground = RichTextEffectBackground.new()
 var _indent :int
 
+
 func _ready():
-	Fonts.init_fonts(self)
+	_update_ui_settings()
 	_effect.set_source(self)
 	# clear effects otherwies a duplicate will result in errors
 	set_effects([])
@@ -19,9 +20,13 @@ func _ready():
 
 func _notification(what):
 	if what == EditorSettings.NOTIFICATION_EDITOR_SETTINGS_CHANGED:
-		Fonts.init_fonts(self)
+		_update_ui_settings()
 		if _effect:
 			_effect._notification(what)
+
+func _update_ui_settings():
+	Fonts.init_fonts(self)
+	updateMinSize()
 
 func set_bbcode(code) -> void:
 	.parse_bbcode(code)
@@ -29,7 +34,9 @@ func set_bbcode(code) -> void:
 
 func append_bbcode(text :String):
 	# replace all tabs, it results in invalid background coloring
-	return .append_bbcode(text.replace("\t", ""))
+	var error := .append_bbcode(text.replace("\t", ""))
+	updateMinSize()
+	return error
 
 func push_indent(indent :int) -> void:
 	.push_indent(indent)
@@ -44,13 +51,15 @@ func pop_indent(indent :int) -> void:
 		_effect.pop_indent(get_line_count(), _indent)
 
 # updates the label minmum size by the longest line content
-# to fit the full test to on line, to avoid line wrapping
+# to fit the full text to on line, to avoid line wrapping
 func updateMinSize() -> void:
-	var min_size := Vector2(0, 0)
+	# reset curren min size
+	rect_min_size.x = 0
+	var font := get("custom_fonts/font") as Font
 	var lines := get_text().split("\n")
 	for line in lines:
-		var chars = line.length() + 1
-		if min_size.x < chars:
-			min_size.x = chars
-	min_size.x *= _effect.get_char_size().x
-	set("rect_min_size", min_size)
+		var line_size := font.get_string_size(line)
+		if rect_min_size < line_size:
+			rect_min_size = line_size
+	# add extra space of 80, the calculated 'get_string_size' not fits right the line wrap size
+	rect_min_size.x += 80
