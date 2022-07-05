3e9abef3dd4a1dfe10c238283021f380bea00084
Nullpointer <mikeschulze.lpz@kabelmail.de>
2022-07-05 21:18:15 +0200

    GD-289: Copy initalisized variables when spy on a instance (#290)
    * GD-289: Copy initalisized variables when spy on a instance
    
    - Spy was not take italisized variables in account,
    - Fixed by copy source values to spy instance when creating the spy

--- addons/gdUnit3/src/spy/GdUnitSpyBuilder.gd
+++ addons/gdUnit3/src/spy/GdUnitSpyBuilder.gd
@@ -143,6 +143,8 @@ static func build(caller :Object, to_spy, push_errors :bool = true, debug_write 
 	if spy == null:
 		return null
 	var spy_instance = spy.new()
+	copy_properties(to_spy, spy_instance)
+	GdUnitObjectInteractions.reset(spy_instance)
 	spy_instance.__set_singleton(to_spy)
 	spy_instance.__set_caller(caller)
 	return GdUnitTools.register_auto_free(spy_instance, memory_pool)
@@ -198,3 +200,20 @@ static func spy_on_scene(caller :Object, scene :Node, memory_pool, debug_write) 
 	scene.set_script(spy)
 	scene.__set_caller(caller)
 	return GdUnitTools.register_auto_free(scene, memory_pool)
+
+const EXCLUDE_PROPERTIES_TO_COPY = ["script", "type"]
+
+static func copy_properties(source :Object, dest :Object) -> void:
+	for property in source.get_property_list():
+		var property_name = property["name"]
+		var property_value = source.get(property_name)
+		if EXCLUDE_PROPERTIES_TO_COPY.has(property_name):
+			continue
+		#if dest.get(property_name) == null:
+		#	prints("|%s|" % property_name, source.get(property_name))
+		
+		# check for invalid name property
+		if property_name == "name" and property_value == "":
+			dest.set(property_name, "<empty>");
+			continue
+		dest.set(property_name, property_value)
