3686f6e7d6c83e26901eb68e963d5afb4431ae2a
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2022-07-23 21:13:58 +0200

    append

--- addons/gdUnit3/scan_project.gd
+++ addons/gdUnit3/scan_project.gd
@@ -1,8 +1,8 @@
-tool
 extends Control
 
 func _ready():
 	print("Scan for project changes ...")
-	yield(get_tree().create_timer(2), "timeout")
+	yield(get_tree().create_timer(30), "timeout")
+	print("Scan for project changes end")
 	get_tree().quit(1)
 	prints(" Done")
