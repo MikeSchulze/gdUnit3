8c71272142fff9d613b4747bdd9f91cde5ecb7c5
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2022-07-24 19:34:23 +0200

    append

--- addons/gdUnit3/scan_project.gd
+++ addons/gdUnit3/scan_project.gd
@@ -13,6 +13,7 @@ var WAIT_TIME_IN_MS = 5.000
 var _state = INIT
 
 func _init():
+	disable_gdUnit()
 	print("Scan for project changes ...")
 	_state = SCAN
 
@@ -25,6 +26,26 @@ func _idle(delta):
 	if _counter >= WAIT_TIME_IN_MS:
 		prints("Scan for project changes done")
 		_state = QUIT
-		#Engine.get_main_loop().finish()
 		#finish()
-		quit(0)
+		rescan()
+		#Engine.get_main_loop().finish()
+		OS.kill(OS.get_process_id())
+
+
+func rescan(update_scripts :bool = false) -> void:
+	yield(root.get_tree(), "idle_frame")
+	var plugin := EditorPlugin.new()
+	var fs := plugin.get_editor_interface().get_resource_filesystem()
+	fs.scan_sources()
+	while fs.is_scanning():
+		yield(root.get_tree().create_timer(1), "timeout")
+	if update_scripts:
+		plugin.get_editor_interface().get_resource_filesystem().update_script_classes()
+	plugin.free()
+
+
+static func disable_gdUnit() -> void:
+	prints("disable_gdUnit")
+	var plugin := EditorPlugin.new()
+	plugin.get_editor_interface().set_plugin_enabled("gdUnit3", false)
+	plugin.free()
