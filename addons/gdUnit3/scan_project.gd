fa74467ce899c3c88ee3806bc8582c7279ccbe88
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2022-07-24 09:11:19 +0200

    append

--- addons/gdUnit3/scan_project.gd
+++ addons/gdUnit3/scan_project.gd
@@ -3,15 +3,16 @@ extends Control
 
 
 var _counter = 0
-var WAIT_TIME_IN_MS = 10.000
+var WAIT_TIME_IN_MS = 5.000
 
 func _ready():
 	print("Scan for project changes ...")
+	prints(OS.get_time())
 
 func _process(delta):
 	_counter += delta
 	if _counter >= WAIT_TIME_IN_MS:
-		print("Scan for project changes done")
+		prints("Scan for project changes done")
 		get_tree().quit(1)
-	prints("scanning", _counter)
+	prints("scanning", _counter, prints(OS.get_time()))
 		
