dbba04b67d14b3add5f33f2961c73c6d8f55b88d
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2023-01-21 21:37:25 +0100

    append

--- addons/gdUnit3/test/core/resources/scenes/simple_scene.gd
+++ addons/gdUnit3/test/core/resources/scenes/simple_scene.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _ready():
-	$Sprite.texture = ResourceLoader.load("res://addons/gdUnit3/test/core/resources/scenes/drag_and_drop/icon.png", "", true)
+	pass # Replace with function body.
